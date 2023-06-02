df494c3aa76af6ec886d769fccc51d69d1d47949
rahimkhan01 <k.rahim@iitg.ac.in>
2023-06-03 01:03:57 +0530

    this is new commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from functions import *
+from flask_cors import CORS,cross_origin
 from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route("/",methods=["POST","GET"])
+@cross_origin()
 def hello():
     if request.method=="POST":
         link=request.form.get('link')
@@ -9,5 +11,5 @@ def hello():
         get_data(link,data)
         return render_template("index.html",data=data)
     return render_template("index.html",data=[[]])
-
-app.run(debug=True,host="0.0.0.0")
+if __name__=="__main__":
+    app.run(debug=True,host="127.0.0.1",port=8000)
