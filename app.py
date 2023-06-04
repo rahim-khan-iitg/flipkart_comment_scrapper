c6bbe3209aa5bce31a87c6434e35e56ea8d2eb36
Rahim Khan <109789890+rahim-khan-iitg@users.noreply.github.com>
2023-06-04 23:48:59 +0530

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def hello():
         return render_template("index.html",data=data)
     return render_template("index.html",data=[[]])
 if __name__=="__main__":
-    app.run(debug=True,host="127.0.0.1",port=8000)
+    app.run(debug=False,host="127.0.0.1",port=8000)
