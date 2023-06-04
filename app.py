66bbd04d11074ffd0d1799c4bbf996e1d2a89ca5
Rahim Khan <109789890+rahim-khan-iitg@users.noreply.github.com>
2023-06-05 00:05:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def hello():
         return render_template("index.html",data=data)
     return render_template("index.html",data=[[]])
 if __name__=="__main__":
-    app.run(debug=False,host="127.0.0.1",port=8000)
+    app.run(debug=False,host="0.0.0.0",port=8080)
